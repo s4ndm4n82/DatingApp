b77f5efe90a99e71c25deb1aabe29b7394dca97c
s4ndm4n82 <sadaruwan12@gmail.com>
2024-12-01 19:40:03 +0530

    Git ignore update.

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ceaf0716269f5d29c19f59631dcf15891aad26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20744931ae77559d2e17ded244380ac8ac78e962")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
